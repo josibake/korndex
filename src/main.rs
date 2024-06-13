6255c4ab7cdc02e9055b864b8d2970c5950cf6c6
josibake <josibake@protonmail.com>
2024-06-13 19:10:34 +0200

    it works!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use lmdb::{Environment, Database, WriteFlags, Transaction, DatabaseFlags};
+use lmdb::{Environment, WriteFlags, Transaction, DatabaseFlags};
 use serde::{Serialize, Deserialize};
 use std::fs;
 use libbitcoinkernel_sys::{
     BlockManagerOptions, ChainType, ChainstateLoadOptions, ChainstateManager,
     ChainstateManagerOptions,
 };
-use bitcoin::Transaction as BitcoinTx;
 use clap::Parser;
 use log::info;
-use std::sync::Arc;
-use std::sync::Mutex;
 use bitcoin::consensus::deserialize;
+use std::path::Path;
 
 mod kernel;
 
@@ -26,14 +24,14 @@ struct Args {
     network: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
 struct TxIndex {
     txid: String,
-    block_height: u64,
+    block_height: i32,
     position_in_block: usize,
 }
+#[derive(Serialize, Deserialize, Debug)]
 struct TxIndexEntry {
-    block_height: u64,
+    block_height: i32,
     position_in_block: usize,
 }
 
@@ -66,12 +64,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     chainman.import_blocks().unwrap();
 
     // Create directory for the LMDB environment
-    let path = "./txindex";
+    let path = Path::new("./txindex");
     fs::create_dir_all(path)?;
 
     // Set up the LMDB environment
     let env = Environment::new()
         .set_max_dbs(10)
+        .set_map_size(1024 * 1024 * 1024)
         .open(path)?;
 
     // Create (or open) a database
@@ -81,57 +80,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
         let mut block_index_res = chainman.get_block_index_tip();
         let mut block_counter = 0;
-
-        let txn = env.begin_rw_txn()?;
-        let txn = Arc::new(Mutex::new(txn));
-
         while let Ok(ref block_index) = block_index_res {
+            let mut txn = env.begin_rw_txn()?;
             let raw_block: Vec<u8> = chainman.read_block_data(&block_index).unwrap().into();
             let block: bitcoin::Block = deserialize(&raw_block).unwrap();
 
             let transactions_data: Vec<TxIndex> = (0..block.txdata.len() - 1)
                 .map(|i| {
-                    let txid: BitcoinTx = block.txdata[i + 1].compute_txid().to_string();
+                    let txid = block.txdata[i + 1].compute_txid();
                     TxIndex {
-                        txid: txid,
+                        txid: txid.to_string(),
                         position_in_block: i,
-                        block_height: 1,
+                        block_height: block_index.info().unwrap().clone().height,
                     }
                 })
                 .collect();
 
 
 
-            transactions_data.par_iter().for_each(|data| {
-                let entry = TxIndexEntry {
-                    position_in_block: data.position_in_block,
-                    block_height: data.block_height,
+            for entry in transactions_data.iter() {
+                let v = TxIndexEntry {
+                    position_in_block: entry.position_in_block,
+                    block_height: entry.block_height,
                 };
-                let serialized = bincode::serialize(&entry);
-                txn.put(db, &data.txid, &serialized, WriteFlags::empty());
+                let serialized = bincode::serialize(&v).unwrap();
+                let _ = txn.put(db, &entry.txid, &serialized, WriteFlags::empty());
 
-            });
+            };
 
             block_index_res = block_index_res.unwrap().prev();
             block_counter += 1;
-            if block_counter % 10 == 0 {
+            if block_counter % 1000 == 0 {
                 info!("Processed block number: {}", block_counter);
             }
+            txn.commit()?;
         }
-        let txn = Arc::try_unwrap(txn).expect("Failed to unwrap Arc").into_inner().unwrap();
-        txn.commit()?;
         log::info!("built index!");
     }
 
     // Example retrieval of a transaction's block location
     {
-        let txid = "example_txid".to_string();
+        let txid = "37d704c8550bf80213ed1b1c3b5798665c7274d67c707bc6e9d6eb4167d3b7f3".to_string();
         let txn = env.begin_ro_txn()?;
         if let Some(data) = txn.get(db, &txid).ok() {
-            let txindex: TxIndex = bincode::deserialize(data)?;
-            println!("Transaction ID: {}, Block Location: {}", txindex.txid, txindex.position_in_block);
-
-            // Here you would use the block location to read the block and extract the full transaction
+            let txindex: TxIndexEntry = bincode::deserialize(data)?;
+            println!("Transaction ID: {}, Block Location: {}", &txid, txindex.position_in_block);
+            let Ok(ref block_index) = chainman.get_block_index_by_height(txindex.block_height) else { todo!() };
+            let raw_block: Vec<u8> = chainman.read_block_data(&block_index).unwrap().into();
+            let block: bitcoin::Block = deserialize(&raw_block).unwrap();
+            let tx = &block.txdata[txindex.position_in_block];
+            println!("full transaction: {:#?}", tx);
         }
     }
 
